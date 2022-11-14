6e6a9286a0fc69a783dd0b7c8ab955c578e62af4
abassimarwen <marwen.abassi@esprit.tn>
2022-11-15 00:13:20 +0100

    webhhook test

--- src/main/java/com/esprit/examen/services/OperateurServiceImpl.java
+++ src/main/java/com/esprit/examen/services/OperateurServiceImpl.java
@@ -40,7 +40,5 @@ public class OperateurServiceImpl implements IOperateurService {
 	public Operateur retrieveOperateur(Long id) {
 		return operateurRepository.findById(id).orElse(null);
 	}
-    public void webhook(){
-		
-	}
+    
 }
